ef97f8fde5d7f3995bebdad1a7afe939ee9cc4b7
Meet <meetnagadia@gmail.com>
2023-12-03 10:09:57 +0530

    added logging into main.py

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 import utils
+import logging
 
+# Setting up Logging
+logging.basicConfig(filename="Log/log.log",
+                    format="%(asctime)s - %(message)s",
+                    datefmt="%d-%b-%y %H:%M:%S",
+                    level=logging.INFO)
+
+logging.info(f"Starting with main.py...")
 URL = 'https://raw.githubusercontent.com/seraph776/DevCommunity/main/PDFDownloader/assests/the_raven.pdf'
 utils.download_pdf_file(URL)
 pdf_path = "pdf_download/the_raven.pdf"
 audio_output_file = "audio_converted/output.mp3"
 text_content = utils.pdf_to_text(pdf_path)
 utils.text_to_audio(text_content, audio_output_file)
+logging.info(f"Ending main.py...")
