92efe2be275fd93b4862cc9dc6b3a8b44c1f6af7
Meet <meetnagadia@gmail.com>
2023-12-03 10:20:34 +0530

    updated main and utils

--- main.py
+++ main.py
@@ -7,11 +7,19 @@ logging.basicConfig(filename="Log/log.log",
                     datefmt="%d-%b-%y %H:%M:%S",
                     level=logging.INFO)
 
-logging.info(f"Starting with main.py...")
-URL = 'https://raw.githubusercontent.com/seraph776/DevCommunity/main/PDFDownloader/assests/the_raven.pdf'
-utils.download_pdf_file(URL)
-pdf_path = "pdf_download/the_raven.pdf"
-audio_output_file = "audio_converted/output.mp3"
-text_content = utils.pdf_to_text(pdf_path)
-utils.text_to_audio(text_content, audio_output_file)
-logging.info(f"Ending main.py...")
+try:
+    logging.info(f"Starting with main.py...")
+
+    # url to the pdf
+    URL = 'https://raw.githubusercontent.com/seraph776/DevCommunity/main/PDFDownloader/assests/the_raven.pdf'
+    utils.download_pdf_file(URL)  # download pdf in local
+    pdf_path = "pdf_download/the_raven.pdf"  # path to store pdf in local
+
+    audio_output_file = "audio_converted/output.mp3"  # path to store audio in local
+    text_content = utils.pdf_to_text(pdf_path)  # extract text from pdf
+    utils.text_to_audio(text_content, audio_output_file)  # convert the text to audio
+
+    logging.info(f"Ending main.py...")
+
+except Exception as e:
+    logging.info(f"Error in main.py: {e}")
