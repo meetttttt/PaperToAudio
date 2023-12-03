a428176abe969fb5aafa00905032a0ff475067bf
Meet <meetnagadia@gmail.com>
2023-12-03 09:52:03 +0530

    Updated main & gitignore

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import utils
 
 URL = 'https://raw.githubusercontent.com/seraph776/DevCommunity/main/PDFDownloader/assests/the_raven.pdf'
 utils.download_pdf_file(URL)
-pdf_path = "download/the_raven.pdf"
-audio_output_file = "output.mp3"
+pdf_path = "pdf_download/the_raven.pdf"
+audio_output_file = "audio_converted/output.mp3"
 text_content = utils.pdf_to_text(pdf_path)
 utils.text_to_audio(text_content, audio_output_file)
